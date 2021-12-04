81e79f414094c2b050fda4b7774db8b17f762ec0
VictoryJu <kob0805@naver.com>
2021-12-04 22:46:29 +0900

    server 라우터 Add:  1. 메세지 라우터 설정  2. 유저 라우터 설정  3. 메세지 , 유저 조회 기능 추가  4. 메세지 수정 삭제 등록 기능 추가

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5233822a4f2b0aa9"; }
+/******/ 		__webpack_require__.h = function() { return "b60778219aa1b709"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -1,73 +1,78 @@
-import MsgItem from "./MsgItem"
+import MsgItem from "./MsgItem";
 import MsgInput from "./MsgInput";
 import { useState } from "react";
 
-const UserIds = ['roy','jay']
-const getRendomUserId = () => UserIds[Math.round(Math.random())] // 랜덤으로 0아니면 1이 나옴 그러면 로이나 제이 중에 하나가 골라짐
+const UserIds = ["roy", "jay"];
+const getRendomUserId = () => UserIds[Math.round(Math.random())]; // 랜덤으로 0아니면 1이 나옴 그러면 로이나 제이 중에 하나가 골라짐
 
-const originalMsgs = Array(50).fill(0).map((_,i)=> ({
-  id: i + 1,
-  userId: getRendomUserId(),
-  timestamp: 1234567890123 + i * 1000 * 60,
-  text: `${i + 1} mock text`
-})).reverse();
+const originalMsgs = Array(50)
+  .fill(0)
+  .map((_, i) => ({
+    id: (i + 1).toString(),
+    userId: getRendomUserId(),
+    timestamp: 1234567890123 + i * 1000 * 60,
+    text: `${i + 1} mock text`,
+  }))
+  .reverse();
+
+console.log(JSON.stringify(originalMsgs));
 
 const MsgList = () => {
-  const [msgs, setMsgs] = useState(originalMsgs)
-  const [editingId, setEditingId] = useState(null)
-  const onCreate = text =>{
+  const [msgs, setMsgs] = useState(originalMsgs);
+  const [editingId, setEditingId] = useState(null);
+  const onCreate = (text) => {
     const newMsg = {
-      id: msgs.length+1,
+      id: msgs.length + 1,
       userId: getRendomUserId(),
       timestamp: Date.now(),
-      text: `${msgs.length+1} ${text}`
-    }
-    setMsgs(msgs => ([newMsg, ...msgs]));
-  }
+      text: `${msgs.length + 1} ${text}`,
+    };
+    setMsgs((msgs) => [newMsg, ...msgs]);
+  };
 
-  const onUpdate = (text,id) =>{
-    setMsgs(msgs =>{
-      const targetIndex = msgs.findIndex(msg => msg.id === id)
-      if(targetIndex < 0) return;  
-      const newMsgs = [...msgs]
-      newMsgs.splice(targetIndex, 1,{
+  const onUpdate = (text, id) => {
+    setMsgs((msgs) => {
+      const targetIndex = msgs.findIndex((msg) => msg.id === id);
+      if (targetIndex < 0) return;
+      const newMsgs = [...msgs];
+      newMsgs.splice(targetIndex, 1, {
         ...msgs[targetIndex],
-        text
-      })
-      return newMsgs
-    })
+        text,
+      });
+      return newMsgs;
+    });
     doneEdit();
-  }
+  };
 
   const doneEdit = () => setEditingId(null);
 
-  const onDelete = (id)=>{
-    setMsgs(msgs =>{
-      const targetIndex = msgs.findIndex(msg => msg.id === id)
-      if(targetIndex < 0) return;
-      const newMsgs = [...msgs]
-      newMsgs.splice(targetIndex, 1)
-      return newMsgs
-    })
-  }
+  const onDelete = (id) => {
+    setMsgs((msgs) => {
+      const targetIndex = msgs.findIndex((msg) => msg.id === id);
+      if (targetIndex < 0) return;
+      const newMsgs = [...msgs];
+      newMsgs.splice(targetIndex, 1);
+      return newMsgs;
+    });
+  };
 
   return (
     <>
-      <MsgInput mutate={onCreate}/>
+      <MsgInput mutate={onCreate} />
       <ul className="messages">
-        {msgs.map(item=> (
-          <MsgItem key={item.id} {...item} 
-          onUpdate={onUpdate}
-          onDelete={() => onDelete(item.id)}
-          startEdit={()=> setEditingId(item.id)}
-          isEdting={editingId === item.id}
+        {msgs.map((item) => (
+          <MsgItem
+            key={item.id}
+            {...item}
+            onUpdate={onUpdate}
+            onDelete={() => onDelete(item.id)}
+            startEdit={() => setEditingId(item.id)}
+            isEdting={editingId === item.id}
           />
         ))}
       </ul>
     </>
-  )
-}
+  );
+};
 
-
-
-export default MsgList
+export default MsgList;
